4d752da84c453a4fe5b6ff9fdbf96220b5521c2e
Mrw0lf14 <43212019+Mrw0lf14@users.noreply.github.com>
2019-12-08 11:56:56 +0300

    Create signin.php

---
+++ signin.php
@@ -0,0 +1,52 @@
+<?php 
+	session_start();
+	if (isset($_SESSION['username'])){
+		header("Location: list.php");
+		exit();
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>registr</title>
+</head>
+<body>
+	<h1>Login</h1>
+    <form action ="" method="post">
+    	<p>login:<input type="text" name="name"></p>
+    	<p>password:<input type="password" name="password"></p>
+    	<p><input type="submit"></p>
+  	</form>
+</body>
+</html>
+<?php
+	
+	$username = $_POST['name'];
+	$pass = $_POST['password'];
+
+	$host='localhost'; // имя хоста (уточняется у провайдера)
+	$database='homework'; // имя базы данных, которую вы должны создать
+	$user='root'; // заданное вами имя пользователя, либо определенное провайдером
+	$table='sem11';//название таблицы
+	$pswd='gavl228_A'; // заданный вами пароль
+
+	$link = mysqli_connect($host, $user, $pswd, $database) or die("Ошибка " . mysqli_error($link));// подключаемся к серверу
+
+	if (isset($_POST['name']) )
+	{
+		$query = "SELECT * FROM $table WHERE name ='$username'";//создаем запрос
+		$res = mysqli_query($link ,$query);//выполняем запрос
+		echo "sdsd";
+
+		if ($res){
+			$row = mysqli_fetch_array($res);//вытаскиваем данные из запроса
+			if ($row['name']=="$username" && $row['password']=="$pass"){
+				$_SESSION['username'] = $username;
+				header("Location: list.php");
+			}
+			else{
+				echo "Wrong password or name";
+			}
+		}
+	}
+?>
